5a580654951836fcddfc1685276e4e40ea7bec49
Ryota Nishida <24.ryota.sub@gmail.com>
2023-03-19 22:58:47 +0900

    修正

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,63 @@
 import React from 'react';
 import Header from './Header';
-import '../css/Login.scss';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 function Login() {
+  const {register, handleSubmit, formState: { errors }} = useForm()
+  const [cookies, setCookie, removeCookie] = useCookies()
+  const onSubmit = (e) => {
+      const data = ({
+        email: e.email,
+        password: e.password
+      })
+
+      axios.post('https://ifrbzeaz2b.execute-api.ap-northeast-1.amazonaws.com/signin', data)
+      .then((res) => {
+        const token = res.data.token;
+        setCookie("token", token)
+      })
+      .catch((err) => {
+        console.log('error!')
+      })
+  }
+
   return (
     <>
       <Header />
       <div className='container'>
-        <p>Login</p>
-        <form className='login-form'>
-          <label>ID</label>
-          <input className='id-area' name="id" type="text" /><br />
-          <label>e-mail</label>
-          <input className='email-area' name="email" type="email" />
-          <p><button className='send-btn'>send</button></p>
+        <p>LOGIN</p>
+
+        <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
+
+          <ul>
+            <li className='input-area'>
+              <label>e-mail</label>
+              <input
+                className='email-area'
+                {...register('email', {required: 'メールアドレスを入力してください'})}
+                type="email"
+              />
+                {errors.email?.message && <p className='required-errmsg'>{errors.email.message}</p>}
+            </li>
+
+            <li className='input-area'>
+              <label>password</label>
+              <input
+                className='password-area'
+                {...register('password', {required: 'パスワードを入力してください'})}
+                type="password"
+              />
+                {errors.password?.message && <p className='required-errmsg'>{errors.password.message}</p>}
+            </li>
+          </ul>
+
+          <p><button className='send-btn'>login</button></p>
+
         </form>
+        <p>ユーザー登録は<Link to="/signup" >こちら</Link></p>
       </div>
     </>
   )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,26 +4,26 @@ import { useState } from 'react';
 import { useCookies } from "react-cookie";
 import { useNavigate, Navigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import axios from 'axios'
-import Header from './Header'
-import '../css/Signup.scss'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Header from './Header';
 
 function Signup() {
   const navigate = useNavigate('')
   const [errorMessage, setErrorMessage] = useState('')
   const {register, handleSubmit, formState: { errors }} = useForm('')
   const [cookies, setCookie, removeCookie] = useCookies()
-  const onSubmit = (data) => {console.log(data)}
+  const url = {process.env.REACT_APP_API_URL};
+  console.log(url)
 
-  const onSignUp = (e) => {
-    // e.preventDefault();
-    // const data = ({
-    //   name: nameRef.current.value,
-    //   email: emailRef.current.value,
-    //   password: passwordRef.current.value,
-    // })
+  const onSubmit = (e) => {
+    const data = ({
+      name: e.name,
+      email: e.email,
+      password: e.password
+    })
 
-    axios.post('https://ifrbzeaz2b.execute-api.ap-northeast-1.amazonaws.com/users')
+    axios.post(`${url}/users`, data)
     .then((res) => {
       const token = res.data.token;
       setCookie("token", token)
@@ -42,54 +42,69 @@ function Signup() {
         <p className='head-text'>Signup</p>
 
         <form className='signup-form' onSubmit={handleSubmit(onSubmit)}>
+          <ul>
+            <li className='input-area'>
+              <label>お名前</label>
+              <input
+                className='name-area'
+                {...register('name', {required: 'お名前は入力必須項目です' })}
+                type="text"
+                placeholder="必須"
+                />
+                {errors.name?.message && <p className='required-errmsg'>{errors.name.message}</p>}
+              <br />
+            </li>
 
-          <label>name</label>
-          <input
-            className='name-area'
-            // {...register('name', {required: 'nameは入力必須項目です' })}
-            type="text"
-            placeholder="required"
-            />
-            {/* {alert(errors.name?.message)} */}
-          <br />
+            <li className='input-area'>
+              <label>e-mail</label>
+              <input
+                className='email-area'
+                {...register('email', {required: 'e-mailは入力必須項目です' })}
+                type="email"
+                placeholder="example@email.jp"
+              />
+                {errors.email?.message && <p className='required-errmsg'>{errors.email.message}</p>}
+              <br />
+            </li>
 
-          <label>e-mail</label>
-          <input
-            className='email-area'
-            // {...register('email', {required: 'emailは入力必須項目です' })}
-            type="email"
-            placeholder="example@email.jp"
-          />
-            {/* {alert(errors.email?.message)} */}
-          <br />
+            <li className='input-area'>
+              <label>パスワード</label>
+              <input
+                className='password-area'
+                {...register('password', {
+                  required: {
+                    value: true,
+                    message: 'パスワードは入力必須項目です'
+                  },
+                  minLength: {
+                    value: 5,
+                    message: 'パスワードは5文字以上入力してください'
+                  }
+                })}
+                type="password"
+                placeholder="必須"
+              />
+                {errors.password?.message && <p className='required-errmsg'>{errors.password.message}</p>}
+              <br />
+            </li>
 
-          <label>password</label>
-          <input
-            className='password-area'
-            {...register('password', {
-              required: {
-                value: true,
-                message: 'passwordは入力必須項目です'
-              },
-              minLength: {
-                value: 5,
-                message: '5文字以上入力してください'
-              }
-            })}
-            type="password"
-            placeholder="required"
-          />
+            <li className='input-area'>
+              <label>アイコン画像(png,jpg)</label>
+              <input
+                className='fileupload-area'
+                {...register('file', {required: 'アイコン画像をアップロードしてください' })}
+                type="file"
+                accept=".png, .jpg"
+              />
+                {errors.file?.message && <p className='required-errmsg'>{errors.file.message}</p>}
+            </li>
 
-          <label>icon imag(png,jpg)</label>
-          <input
-            className='fileupload-area'
-            {...register('file', {required: true })}
-            type="file"
-            accept=".png, .jpg"
-          />
-
-          <p><button className='send-btn'>send</button></p>
+            <p><button className='send-btn'>send</button></p>
+          </ul>
         </form>
+        <Link to="/login">
+          ログインへ
+        </Link>
       </div>
     </>
   )
